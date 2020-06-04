9bb242d359a61e298f3d08581937c2d0ff7ffc22
Farhan Khan <farhan.khan@10pearls.local>
2020-06-04 23:29:46 +0500

    fix assertion for events

--- cypress/integration/resources.spec.js
+++ cypress/integration/resources.spec.js
@@ -12,27 +12,22 @@ describe('hobsons.com homesite verification', () => {
     })
 
     it('Verify Events', function () {
-        let eventData = [
-            "FADSS Winter Leadership Conference",
-            "NSSR (National Symposium on Student Retention)",
-            "APLU Annual Meeting",
-            "NACADA 2019",
-            "ERDI Winter 2020",
-            "AASA NCE 2020",
-            "TASA 2020",
-            "CGCS 2019",
-            "ERDI Summer 2020"
-        ]
         cy.get('.menu').click()
         cy.contains('Resources').click()
         cy.contains('Events').click()
-        // The following events have been hardcoded in eventData
-        // due to the lack of a source of truth for the events.
-        // can fail if the events are updated.
-        let length = eventData.length
-        for (let i = 0; i < length; i++) {
-            cy.get('.res-events > .txt > h4').should('contain', eventData[i])
-        }
+        let currentYear = new Date().getFullYear()
+        cy.get('.res-events').then((events) => {
+            for (let i = 0; i < events.length; i++) {
+                cy.get('.res-events > .txt').eq(i).then((elem) => {
+                    let text = elem.text()
+                    // the condition is added to filter out all events from the current year
+                    // if asserted without condition, the test will fail on non current year events
+                    if (text.includes(currentYear)) {
+                        cy.wrap(elem.text()).should('contain', currentYear)
+                    }
+                })
+            }
+        })
     })
 
 })
